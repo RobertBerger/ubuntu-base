3711d29d83367a590b8f367de23e2d13e6e21779
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-05-30 09:26:44 +0300

    tun device permissions

--- dockerfile/etc_my_init.d/01_create_tun.sh
+++ dockerfile/etc_my_init.d/01_create_tun.sh
@@ -4,3 +4,5 @@ if [[ ! -e /dev/net/tun ]]; then
         echo "tun node does not exist. Generating it ..."
         (HERE=$(pwd) && cd /dev && MAKEDEV tun && cd ${HERE})
 fi
+# we need to fix permissions
+sudo chmod 666 /dev/net/tun
