30d80aba31c34d07ddbf61ca8a9cdd5950ebdc7d
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-06-02 22:11:07 +0300

    manual merge of phusion

--- non-local_scripts/docker_run.sh
+++ non-local_scripts/docker_run.sh
@@ -6,23 +6,25 @@ then
     echo "+ $0: Too few arguments!"
     echo "+ use something like:"
     echo "+ $0 <docker image> <network interface>" 
+    echo "+ $0 reliableembeddedsystems/ubuntu-base docker0"
     echo "+ $0 reliableembeddedsystems/ubuntu-base br0"
     exit
 fi
 
 # enable TUN device (for qemu)
-# echo "+ sudo modprobe tun"
-# sudo modprobe tun
+echo "+ sudo modprobe tun"
+sudo modprobe tun
 
-# run the image
-echo "+ ID=\$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)"
-ID=$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)
+echo "+ ID=\$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)"
+ID=$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)
+
+echo "+ ID ${ID}"
 
 # ssh stuff:
 PORT=$(docker port ${ID} 22 | awk -F':' '{ print $2 }')
 IPADDR=$(ifconfig ${NETWORK_INTERFACE} | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 echo "+ ssh to the container like this:"
-echo "ssh genius@${IPADDR} -p ${PORT}"
+echo "ssh -X genius@${IPADDR} -p ${PORT}"
 
 # let's attach to it:
 echo "+ docker attach ${ID}"
