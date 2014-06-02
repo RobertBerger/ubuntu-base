e8c70d374005c42ff28511ce1c49b6a9c840fca5
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-06-02 21:56:44 +0300

    merge

--- dockerfile/etc_my_init.d/01_create_tun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-if [[ ! -e /dev/net/tun ]]; then
-        echo "tun node does not exist. Generating it ..."
-        (HERE=$(pwd) && cd /dev && MAKEDEV tun && cd ${HERE})
-fi
-# we need to fix permissions
-sudo chmod 666 /dev/net/tun
--- local_scripts/docker_run.sh
+++ local_scripts/docker_run.sh
@@ -6,34 +6,24 @@ then
     echo "+ $0: Too few arguments!"
     echo "+ use something like:"
     echo "+ $0 <docker image> <network interface>" 
-    echo "+ $0 reslocal/ubuntu-base docker0"
     echo "+ $0 reslocal/ubuntu-base br0"
     exit
 fi
 
 # enable TUN device (for qemu)
-echo "+ sudo modprobe tun"
-sudo modprobe tun
+# echo "+ sudo modprobe tun"
+# sudo modprobe tun
 
 # run the image
-#echo "+ ID=\$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)"
-#ID=$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)
-
-#echo "+ ID=\$(docker run --rm -t -i ${IMAGE_NAME} /sbin/my_init -- bash -l)"
-#ID=$(docker run --rm -t -i ${IMAGE_NAME} /sbin/my_init -- bash -l)
-
-echo "+ ID=\$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)"
-ID=$(docker run -t -i -d -p 22 --privileged ${IMAGE_NAME} /sbin/my_init -- bash -l)
-
-echo "+ ID ${ID}"
+echo "+ ID=\$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)"
+ID=$(docker run -i -t -d -p 22 --privileged ${IMAGE_NAME} /bin/bash)
 
 # ssh stuff:
 PORT=$(docker port ${ID} 22 | awk -F':' '{ print $2 }')
 IPADDR=$(ifconfig ${NETWORK_INTERFACE} | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 echo "+ ssh to the container like this:"
-echo "ssh -X genius@${IPADDR} -p ${PORT}"
+echo "ssh genius@${IPADDR} -p ${PORT}"
 
 # let's attach to it:
 echo "+ docker attach ${ID}"
 docker attach ${ID}
-
